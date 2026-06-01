99e074111dfed27068c49e7b7d42ca5bdb9d60dc
Nagaswapna20 <kalluvapalliswapna@gmail.com>
2026-06-01 15:10:19 +0530

    Add files via upload

---
+++ Rotate_array.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+class Rotate_array
+{
+	public static void main(String args[])
+	{
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
+		int[] arr=new int[n];
+		for(int i=0;i<n;i++)
+		{
+			arr[i]=s.nextInt();
+		}
+		int k=s.nextInt();
+		for(int i=0;i<k;i++)
+		{
+			int temp=arr[n-1];
+			for(int j=n-1;j>0;j--)
+			{
+				arr[j]=arr[j-1];
+			}
+			arr[0]=temp;
+		}
+		for(int i=0;i<n;i++)
+		{
+			System.out.print(arr[i]+" ");
+		}
+	}
+}
